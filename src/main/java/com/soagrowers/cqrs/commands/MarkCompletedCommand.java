1f4099202f54aca5954b2129c9dc904b7426e55f
benwilcock <benwilcock@gmail.com>
2015-08-07 17:23:31 +0100

    more docs to explain what's going on

--- src/main/java/com/soagrowers/cqrs/commands/MarkCompletedCommand.java
+++ src/main/java/com/soagrowers/cqrs/commands/MarkCompletedCommand.java
@@ -7,9 +7,19 @@ import org.axonframework.commandhandling.annotation.TargetAggregateIdentifier;
  */
 public class MarkCompletedCommand {
 
+    /**
+     * How does Axon know which Aggregate to Mark as completed? It uses
+     * the TargetAggregateIdentifier annotation so that it can get the
+     * correct one based on the Id in the annotated field.
+     */
     @TargetAggregateIdentifier
     private final String todoId;
 
+    /**
+     * This constructor must set the Id field, otherwise it's unclear
+     * to Axon which aggregate this command is intended for.
+     * @param todoId
+     */
     public MarkCompletedCommand(String todoId) {
         this.todoId = todoId;
     }
