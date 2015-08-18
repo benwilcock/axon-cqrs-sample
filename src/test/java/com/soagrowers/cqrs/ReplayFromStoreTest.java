2d5130061ed43bfc68d1ef993882c3fee4496f57
benwilcock <benwilcock@gmail.com>
2015-08-18 16:21:14 +0100

    more sophisticated replay tests including "from dateTime"

--- src/test/java/com/soagrowers/cqrs/ReplayFromStoreTest.java
+++ src/test/java/com/soagrowers/cqrs/ReplayFromStoreTest.java
@@ -6,16 +6,17 @@ import com.soagrowers.cqrs.commands.CreateToDoItemCommand;
 import com.soagrowers.cqrs.commands.MarkCompletedCommand;
 import com.soagrowers.cqrs.views.MaterialView;
 import org.axonframework.eventhandling.replay.ReplayingCluster;
+import org.axonframework.eventstore.management.Criteria;
 import org.axonframework.eventstore.management.CriteriaBuilder;
 import org.axonframework.eventstore.mongo.DefaultMongoTemplate;
 import org.axonframework.eventstore.mongo.MongoEventStore;
+import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -71,9 +72,53 @@ public class ReplayFromStoreTest {
         assertEquals(numberOfToDoItems, MaterialView.getInstance().getDoneCount());
     }
 
+    @Test
+    public void testPartialReplaysForIdEquals() {
+        assertFalse(this.cluster.isInReplayMode());
+
+        CriteriaBuilder cb = this.cluster.newCriteriaBuilder();
+        Criteria criteria = cb.property("aggregateIdentifier").is("5");
+        this.cluster.startReplay(criteria);
+
+        assertEquals(1, MaterialView.getInstance().getDoneCount());
+    }
+
+    @Test
+    public void testPartialReplaysForIdGreaterThan() {
+        assertFalse(this.cluster.isInReplayMode());
+
+        CriteriaBuilder cb = this.cluster.newCriteriaBuilder();
+        Criteria criteria = cb.property("aggregateIdentifier").greaterThanEquals("5");
+        this.cluster.startReplay(criteria);
+
+        assertEquals(5, MaterialView.getInstance().getDoneCount());
+    }
+
+    @Test
+    public void testPartialReplaysForIdGreaterThanXAndLessThanY() {
+        assertFalse(this.cluster.isInReplayMode());
+
+        CriteriaBuilder cb = this.cluster.newCriteriaBuilder();
+        Criteria criteria =
+                cb.property("aggregateIdentifier").greaterThanEquals("5")
+                .and(cb.property("aggregateIdentifier").lessThanEquals("8"));
+        this.cluster.startReplay(criteria);
+
+        assertEquals(4, MaterialView.getInstance().getDoneCount());
+    }
+
+    @Test
+    public void testPartialReplaysFromTime() {
+        assertFalse(this.cluster.isInReplayMode());
+        DateTime timestamp = this.store.readEvents("test", "5").next().getTimestamp();
+        CriteriaBuilder cb = this.cluster.newCriteriaBuilder();
+        Criteria criteria = cb.property("timeStamp").greaterThanEquals(timestamp);
+        this.cluster.startReplay(criteria);
+        assertEquals(6, MaterialView.getInstance().getDoneCount());
+    }
+
     @After
     public void checkLog() {
-
-        //MaterialView.getInstance().dumpView();
+        MaterialView.getInstance().dumpView();
     }
 }
